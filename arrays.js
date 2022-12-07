080963738276d5cc507745b48007d09bd2e9b894
Elise Jones <02elise.jones02@gmail.com>
2022-12-07 07:47:04 -0700

    Will add functions to arrays

--- arrays.js
+++ arrays.js
@@ -1,5 +1,21 @@
-var shirtSizes = ["Small", "Medium", "Large", "Extra Large"]
+var shirtSizes = ["Small", "Medium", "Large", "Extra Large"];
+//This line of code calls the pop method of shirtSizes and will remove the last element which in this case 
+// is a Extra Large
+shirtSizes.pop();
+console.log(shirtSizes);
 
-var summerTemps = [92, 88, 94, 100]
+var summerTemps = [ 92, 88, 94, 100 ]; 
+// This line of code calls the shift function on summerTemps which will remove the the element in the
+//first  element in the array.
+summerTemps.shift();
+console.log(summerTemps);
 
-var seniorGrades [true, false , false, false]
+var seniorGrades = [true, false, false, false];
+// This line of code calls the revers function on seniorGrades which reverses the order
+// of the elements in the array.
+seniorGrades.reverse();
+console.log(seniorGrades);
+
+
+
+
